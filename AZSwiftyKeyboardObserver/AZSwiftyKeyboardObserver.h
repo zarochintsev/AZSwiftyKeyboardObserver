b7ec0532a7bc9fd2c2e90a35b08b465a29a4eaa6
Alex Zarochintsev <alex.zarochintsev@gmail.com>
2018-02-13 18:48:11 +0200

    Initial Commit

---
+++ AZSwiftyKeyboardObserver/AZSwiftyKeyboardObserver.h
@@ -0,0 +1,19 @@
+//
+//  AZSwiftyKeyboardObserver.h
+//  AZSwiftyKeyboardObserver
+//
+//  Created by Alex Zarochintsev on 2/13/18.
+//  Copyright © 2018 [Zarochintsev Production];. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AZSwiftyKeyboardObserver.
+FOUNDATION_EXPORT double AZSwiftyKeyboardObserverVersionNumber;
+
+//! Project version string for AZSwiftyKeyboardObserver.
+FOUNDATION_EXPORT const unsigned char AZSwiftyKeyboardObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZSwiftyKeyboardObserver/PublicHeader.h>
+
+
